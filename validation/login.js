86ad8b169df7d1b55fcad9adafee13edf21007c1
Your Name <nazik_111@mail.ru>
2018-05-04 17:28:28 +0500

    registration complete

--- validation/login.js
+++ validation/login.js
@@ -1,20 +1,10 @@
 const Validator = require('validator');
 const isEmpty = require('./is-empty');
 
-module.exports = function validateRegisterInput(data) {
+module.exports = function validateLoginInput(data) {
   errors = {};
-  data.name = !isEmpty(data.name) ? data.name : '';
   data.email = !isEmpty(data.email) ? data.email : '';
   data.password = !isEmpty(data.password) ? data.password : '';
-  data.password2 = !isEmpty(data.password2) ? data.password2 : '';
-
-  if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
-    errors.name = 'Name must be between 2 and 30 characters';
-  }
-
-  if (Validator.isEmpty(data.name)) {
-    errors.name = 'Name field is required';
-  }
 
   if (!Validator.isEmail(data.email)) {
     errors.email = 'Email is invalid';
@@ -24,20 +14,8 @@ module.exports = function validateRegisterInput(data) {
     errors.email = 'Email field is required';
   }
 
-  if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
-    errors.password = 'Password must be at least 6 characters';
-  }
-
   if (Validator.isEmpty(data.password)) {
     errors.password = 'Password Field is required';
-  }
-
-  if (!Validator.equals(data.password, data.password2)) {
-    errors.password2 = 'Passwords must match';
-  }
-
-  if (Validator.isEmpty(data.password2)) {
-    errors.password2 = 'Confirm Password field is required';
   }
 
   return {
