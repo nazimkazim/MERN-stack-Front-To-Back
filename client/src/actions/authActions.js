1050f5fd66bafdc42919aac7eaf61ea7b3825ec6
Your Name <nazik_111@mail.ru>
2018-05-24 20:22:30 +0500

    add setAuthToken

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,6 +1,8 @@
-import { GET_ERRORS } from './types';
 import axios from 'axios';
+import setAuthToken from '../utils/setAuthToken';
+import jwt_decode from 'jwt-decode';
 
+import { GET_ERRORS, SET_CURRENT_USER } from './types';
 // Register user
 export const registerUser = (userData, history) => dispatch => {
   axios
@@ -12,4 +14,40 @@ export const registerUser = (userData, history) => dispatch => {
         payload: err.response.data
       })
     );
+};
+
+// Login get User Token
+export const loginUser = userData => dispatch => {
+  axios
+    .post('/api/users/login', userData)
+    .then(res => {
+      // Save to Local Storage
+      const { token } = res.data;
+
+      // Set token to Local Storage
+      localStorage.setItem('jwtToken', token);
+
+      // Set token to auth header
+      setAuthToken(token);
+
+      // Decode token to get user data
+      const decoded = jwt_decode(token);
+
+      // Set current user
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Set logged in user
+export const setCurrentUser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  };
 };
